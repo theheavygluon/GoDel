7fb70b27646436203eb7bb2ce213832979d42eae
kimutaiRop <kimrop20@gmail.com>
2021-11-15 17:21:00 +0300

    add matrix with inputs and style matrix

--- example/main.go
+++ example/main.go
@@ -2,9 +2,7 @@ package main
 
 import (
 	"GoDel"
-	"fmt"
 	"gonum.org/v1/gonum/mat"
-	"math"
 )
 
 func main() {
@@ -12,18 +10,17 @@ func main() {
 	qc.ApplyGate(GoDel.H, 0)
 	qc.ApplyGate(GoDel.Z, 1)
 	qc.ApplyGate(GoDel.X, 1)
-	fmt.Println(GoDel.H.MulElem(1/math.Sqrt(2)))
-	fmt.Println(GoDel.H.Caps()) // get domension of matrix
-	fmt.Println(GoDel.H.ColView(0)) // view first col of H
-	//TODO
-	// find a way to make the matrix construction short
-	var precustomMatrixData = make([]float64, 6)
-	var customMatrixData = [6]float64{1, 1, 1, 1, 1, 1}
-	for i := range customMatrixData {
-		precustomMatrixData[i] = customMatrixData[i]
-	}
-	// END HERE
-	customMatrix := mat.NewDense(3, 2, precustomMatrixData)
+	qc.ApplyGate(GoDel.CN, 1)
+	qc.ApplyGate(GoDel.CXX(120), 1) // using matrix with other params
+
+	//create custom matrix and add to gates
+	customMatrix := mat.NewDense(3, 2, []float64{
+		1, 1,
+		1, 1,
+		1, 1,
+	})
 	qc.ApplyGate(customMatrix, 1) // add custom matrix to the gates
-	fmt.Println(qc.Gates)
+
+	// end here
+
 }
--- quantumCiruit.go
+++ quantumCiruit.go
@@ -4,6 +4,7 @@ import (
 	"errors"
 	"fmt"
 	"gonum.org/v1/gonum/mat"
+	"math"
 )
 
 type Gate struct {
@@ -15,25 +16,64 @@ type QuantumCircuit struct {
 	Qubits int
 }
 
-// H create matrix H
-var H = mat.NewDense(2, 2, []float64{1, 1, 1, -1})
+// H create matrix H (haddaman gate)
+/*
+Wll be scalled with (1 / sqrt(2)) -> (1 / sqrt(2)) * H
+*/
+var H = mat.NewDense(2, 2, []float64{
+	1, 1,
+	1, -1,
+})
 
 // end of h
 
-// X create matrix X
-var X = mat.NewDense(2, 2, []float64{0, 1, 1, 0})
-
+// X
+/*
+create matrix X (basic NOT gate)
+ */
+var X = mat.NewDense(2, 2, []float64{
+	0, 1,
+	1, 0,
+})
 // end of x
 
 // Z create matrix Z
+/*
+	matrix z
+ */
 var Z = mat.NewDense(2, 2, []float64{1, 0, 0, -1})
+// end of z
+
+// CN create matrix CN (controled not gate)
+/*
+this will flip target qubit if controlled qubit is set to 1 else no change to target qubit
+*/
+var CN = mat.NewDense(4, 4, []float64{
+	1, 0, 0, 0,
+	0, 1, 0, 0,
+	0, 0, 0, 1,
+	0, 0, 1, 0,
+})
 
 // end of z
 
-func init() {
-
+// CXX
+/*craete amtrix CXX that also accepts inputs
+ */
+func CXX(pie float64) mat.Matrix {
+	fmt.Println(pie)
+	return mat.NewDense(2, 2, []float64{1, 0, 0, -1})
 }
 
+func init() {
+	H.Scale(1/math.Sqrt(2), H) // scale H by (1 / sqrt(2))
+}
+
+//ApplyGate
+/*
+ applyGate contructs a gate, accepts(matrix for the gate and also qubit to apply to)
+if the qubit specified is our of range the program will panic
+*/
 func (qc *QuantumCircuit) ApplyGate(gate mat.Matrix, qubit int) {
 	if qubit >= qc.Qubits {
 		err := errors.New("Qubits: qubit out of range for  the circuit")
